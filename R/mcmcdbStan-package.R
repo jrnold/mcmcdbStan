17850b95429777871dc16fb451b02ba9b36bad55
Jeff Arnold <jeffrey.arnold@gmail.com>
2013-05-04 09:56:52 -0400

    initial commit

---
+++ R/mcmcdbStan-package.R
@@ -0,0 +1,10 @@
+#' mcmcdbStan package
+#'
+#' Extends \pkg{mcmcdb} to read \pkg{rstan} output.
+#'
+#' @name mcmcdbStan
+#' @docType package
+#' @import mcmcdb
+#' @import plyr
+#' @importClassesFrom rstan stanfit
+NULL
